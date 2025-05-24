27fd98ecbdf7c3ff94226cb9cd1bd11d3604a7dc
Aman Singh <aman09singh15@gmail.com>
2025-05-24 21:57:35 +0530

    Added HATEOAS - HyperMedia as the Engine for Application State standard

--- RestFul-Application/src/main/java/com/restful/app/controller/AccessController.java
+++ RestFul-Application/src/main/java/com/restful/app/controller/AccessController.java
@@ -2,16 +2,19 @@ package com.restful.app.controller;
 
 import com.restful.app.model.ResponseObject;
 import com.restful.app.model.UserAuthDTO;
-import com.restful.app.model.UserDTO;
 import com.restful.app.service.implementation.AuthServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/auth")
@@ -23,13 +26,18 @@ public class AccessController {
     public AccessController(AuthServiceImpl authService) {
         this.authService = authService;
     }
-    
+
     @PostMapping("/validate")
     @Operation(summary = "Authenticates user",
             description = "Authenticates user and returns an object with the Authorization header")
-    public ResponseEntity<ResponseObject> authenticateUserAndGenerateHeader(@RequestBody UserAuthDTO userAuthDTO){
+    public ResponseEntity<EntityModel<ResponseObject>> authenticateUserAndGenerateHeader(@RequestBody UserAuthDTO userAuthDTO){
+
         ResponseObject responseObject = authService.returnAuthenticatedHeader(userAuthDTO);
-        return new ResponseEntity<ResponseObject>(responseObject, HttpStatus.OK);
+
+        EntityModel<ResponseObject> responseObjectEntity =  EntityModel.of(responseObject);
+        responseObjectEntity.add(linkTo(methodOn(AccessController.class).authenticateUserAndGenerateHeader(userAuthDTO)).withSelfRel());
+
+        return new ResponseEntity<EntityModel<ResponseObject>>(responseObjectEntity, HttpStatus.OK);
 
     }
 }
